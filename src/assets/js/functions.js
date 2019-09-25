885cc01a8608f0cf8a7683e669915ea952cfc910
danielHLopes <danielhlopes90@gmail.com>
2019-09-25 11:00:37 -0400

    refeito com estado

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -1,3 +1,170 @@
+let nome = document.querySelector("#nome");
+let peso = document.querySelector("#peso");
+let altura = document.querySelector("#altura");
+
+let tabela = document.querySelector(".table")
+
+document.querySelector("#btn-calcular").addEventListener("click", (event) => {
+    event.preventDefault();
+    let imc = calcularIMC(peso.value, altura.value);
+    let estado = estadoIMC(imc);
+    //addTabela(nome.value, peso.value, altura.value,imc.toFixed(2), estado)
+    addLocalStorage(nome.value, peso.value, altura.value, imc.toFixed(2), estado);
+    carregarLocalStorage();
+    limparFormulario();
+    //console.log(imc.toFixed(2));
+    
+})
+
+
+function calcularIMC(peso, altura){
+    return peso / (altura * altura);
+}
+
+function estadoIMC(imc){
+    let resut;
+    if(imc < 18.5){
+        resut =  "desnutricao";
+    }else if((imc >= 18.5)&&(imc < 24.5)){
+        resut =   "peso normal";
+    }else if((imc>=24.5)&&(imc<30)){
+        resut =   "sobrepeso"
+    }else if((imc>=30)&&(imc<40)){
+        resut =   "obesidade";
+    }else if(imc>=40){
+        resut =   "obesidade morbida";
+    }
+    
+    return resut;
+}
+
+function addTabela(nome, peso, altura, imc, estado, indice){
+    let colunaNome = document.createElement('td');
+    colunaNome.innerHTML = nome;
+
+    let colunaPeso = document.createElement('td');
+    colunaPeso.innerHTML = peso;
+
+    let colunaAltura = document.createElement('td');
+    colunaAltura.innerHTML = altura;
+
+    let colunaIMC = document.createElement('td');
+    colunaIMC.innerHTML = imc;
+
+    let colunaEstado = document.createElement('td');
+    colunaEstado.innerHTML = estado;
+
+
+    let colunaDeletar = document.createElement('td');
+    let btnDeletar = document.createElement('button');
+    btnDeletar.innerHTML = '<img src="assets/images/delete.svg" alt="Deletar IMC">';
+    btnDeletar.classList.add('btn');
+    btnDeletar.classList.add('btn-danger');
+
+    btnDeletar.addEventListener("click", (event) => {
+        event.preventDefault();
+        deletarLinha(indice);
+      });
+
+    colunaDeletar.appendChild(btnDeletar);
+
+
+    let linha = document.createElement('tr');
+    linha.appendChild(colunaNome);
+    linha.appendChild(colunaPeso);
+    linha.appendChild(colunaAltura);
+    linha.appendChild(colunaIMC);
+    linha.appendChild(colunaEstado);
+    linha.appendChild(colunaDeletar);
+
+    tabela.appendChild(linha);
+}
+
+function limparFormulario(){
+    nome.value = '';
+    peso.value = '';
+    altura.value = '';
+    nome.focus();
+  }
+
+function addLocalStorage(nome, peso, altura, imc, estado){
+    let pessoa = {
+        "nome" : nome,
+        "peso" : peso,
+        "altura" : altura,
+        "imc" : imc,
+        "estado" : estado
+    }
+
+    if(localStorage.getItem("listaIMC")){
+        let listaIMC = JSON.parse(localStorage.getItem("listaIMC"));
+        listaIMC.push(pessoa);
+        localStorage.setItem("listaIMC", JSON.stringify(listaIMC));
+
+    }else{
+        let listaIMC = [];
+        listaIMC.push(pessoa);
+        localStorage.setItem("listaIMC", JSON.stringify(listaIMC));
+    }
+
+    mostraMensagem("Perfil cadastrado", "add")
+}
+
+function limparTabela(){
+    let qtdLinhas = tabela.rows.length;
+    for(let i = qtdLinhas -1; i>0;i--){
+        tabela.deleteRow(i);
+    }
+}
+
+function carregarLocalStorage(){
+    limparTabela();
+
+    if(localStorage.getItem("listaIMC")){
+        let listaIMC = JSON.parse(localStorage.getItem("listaIMC"));
+        listaIMC.forEach((pessoa, indice) =>{
+            addTabela(pessoa.nome, pessoa.peso,pessoa.altura,pessoa.imc,pessoa.estado,indice);
+        });
+    }else{
+        mostraMensagem("Nenhum perfil para carregar", "table");
+    }
+}
+
+function deletarLinha(index){
+    let pessoas = JSON.parse(localStorage.getItem("listaIMC"));
+    pessoas.splice(index,1);
+    localStorage.setItem("listaIMC",JSON.stringify(pessoas));
+    carregarLocalStorage();
+
+    mostraMensagem("Perfil deletado", "delete");
+
+}
+
+let mensagem = document.querySelector("#mensagem");
+
+function mostraMensagem(msg, tipo){
+    mensagem.innerHTML = msg;
+    mensagem.classList.add("d-block");
+
+    if(tipo == 'add'){
+        mensagem.classList.add("alert-sucess");
+    }else if(tipo == 'delete'){
+        mensagem.classList.add("alert-danger");
+    }else if(tipo == 'table'){
+        mensagem.classList.add("alert-warnig");
+    }
+
+    setTimeout(() => {
+        mensagem.innerHTML = "";
+        mensagem.classList.remove("alert-danger");
+        mensagem.classList.remove("alert-success");
+        mensagem.classList.remove("alert-warning");
+        mensagem.classList.remove("d-none");
+    }, 2000);
+}
+
+
+/*
 //console.log("ola mundo");imprimir no console
 let nome = document.querySelector("#nome");//criar variavel para receber o campo pelo id dele
 let peso = document.querySelector("#peso");
@@ -139,5 +306,5 @@ document.querySelector("#btn-calcular").addEventListener(//adicionei um evento a
         limparFormulario();
 
     }
-);
+);*/
 //-------------------------------------------------------------------------------------------------------------------------------
